d0de2de6954b153462f539990732be8cf0992571
KzVerif <kanghunproject@gmail.com>
2026-01-09 19:01:52 +0700

    แก้ใหม่

--- lib/requireAdmin.ts
+++ lib/requireAdmin.ts
@@ -5,7 +5,7 @@ import { authOptions } from "./auth";
 export async function requireAdmin() {
   
   const session = await getServerSession(authOptions); 
-  if (!session || session?.user.role !== "ADMIN") {
+  if (!session || session?.user.role === "ADMIN") {
     throw new Error("UNAUTHORIZED");
   } 
 }
